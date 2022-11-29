aa26771fe924e3547b60ec6a12b2454104b41bf5
kakigorimochi <16938949+kakigorimochi@users.noreply.github.com>
2022-11-30 04:18:22 +0800

    UI Updates, Wallet

--- resources/js/pages/commuter/newbalance.vue
+++ resources/js/pages/commuter/newbalance.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="container pt-6 px-6">
-        <div class="is-flex is-flex-direction-column">
-            <p>Balance: <b>PHP {{ newBalance - addedValue }}</b></p>
-            <p>Added Value: <b>PHP {{ addedValue }}</b></p>
-            <p>Total: <b>PHP {{ newBalance }}</b></p>
-        </div>
-        <div class="container is-flex is-justify-content-end p-3">
-            <b-button class="mr-3" @click="index"
-                :loading="isLoading" type="is-success">Done</b-button>
+    <div class="container pt-5 px-5" id="window">
+        <span id="subheader" class="is-size-4 text-centered">E-wallet</span>
+        <div class="container is-flex is-flex-direction-column mt-6">
+            <div class="is-flex is-flex-direction-column">
+                <p>Balance: <b>PHP {{ newBalance - addedValue }}</b></p>
+                <p>Added Value: <b>PHP {{ addedValue }}</b></p>
+                <p>Total: <b>PHP {{ newBalance }}</b></p>
+            </div>
+            <div class="mb-5">
+                <b-button id="back" class="is-medium is-fullwidth"
+                @click="index" :loading="isLoading" type="is-success">Done</b-button>
+            </div>
         </div>
     </div>
 </template>
